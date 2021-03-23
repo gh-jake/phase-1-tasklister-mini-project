16cee06c9f469ad9e1cb95aef13c1b97b2e8a05b
gh-jake <78183639+gh-jake@users.noreply.github.com>
2021-03-22 20:33:39 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let inputValue = document.getElementById("new-task-description").value;
+    let dueDate = document.getElementById("due-date").value;
+    let toDoList = document.getElementById("list");
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(inputValue + " due on " + dueDate));
+    toDoList.appendChild(li);
+
+  });
+
 });
